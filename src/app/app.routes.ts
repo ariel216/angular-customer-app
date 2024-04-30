a65ff537525bf765160499f2f865d2d1909e1ae8
arie216 <arielespinoza216@gmail.com>
2024-04-30 12:42:48 -0400

    add: scaffolding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { StoreComponent } from './features/orders/store/store.component';
+import { ArticlesComponent } from './features/admin/articles/articles.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'orders', component: StoreComponent },
+  { path: '**', component: NotFoundComponent },
+];
---
+++ src/app/shared/pipes/literal-month.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { LiteralMonthPipe } from './literal-month.pipe';
+
+describe('LiteralMonthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new LiteralMonthPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
